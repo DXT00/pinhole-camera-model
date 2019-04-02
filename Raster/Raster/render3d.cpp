1a50ca4dd16d2a6bdc408fb14ab1c130ffc3b28b
DXT00 <dingxiaotong11@yahoo.com>
2019-04-02 15:58:48 +0800

    Update render3d.cpp

--- Raster/Raster/render3d.cpp
+++ Raster/Raster/render3d.cpp
@@ -10,26 +10,26 @@ void Aperture_Image_RatioFit(float &filmApertureWidth, float &filmApertureHeight
 
 	float filmRatio = filmApertureWidth / filmApertureHeight;
 	float imageRatio = imageWidth / imageHeight;
-	vec2f scale(1.0,1.0);
+	vec2f scale(1.0, 1.0);
 	switch (fitMethod)
 	{
-	case(FILL): 
-			if (filmRatio > imageRatio) {
-				scale.x = imageRatio / filmRatio;
-			}
-			else {
-				scale.y = filmRatio/ imageRatio;
+	case(FILL):
+		if (filmRatio > imageRatio) {
+			scale.x = imageRatio / filmRatio;
+		}
+		else {
+			scale.y = filmRatio / imageRatio;
 
-			}
-	break;
-	case(OVERSCAN): 
+		}
+		break;
+	case(OVERSCAN):
 		if (filmRatio > imageRatio) {
 			scale.y = filmRatio / imageRatio;
 		}
 		else {
 			scale.x = imageRatio / filmRatio;
 		}
-	
+
 	default:
 		break;
 	}
@@ -40,9 +40,9 @@ void Aperture_Image_RatioFit(float &filmApertureWidth, float &filmApertureHeight
 }
 
 void computeCanvasSize(const float focalLength, const float filmApertureWidth,
-	const float filmApertureHeight,const float inchToMm,
-	const float nearClipingPlane,const float farClipingPlane,
-	float &left,float &right,float &top,float &bottom,
+	const float filmApertureHeight, const float inchToMm,
+	const float nearClipingPlane, const float farClipingPlane,
+	float &left, float &right, float &top, float &bottom,
 	float &angleOfViewHorizontal,
 	float &angleOfViewVertical,
 	float &filmAspectRatio) {
@@ -55,19 +55,19 @@ void computeCanvasSize(const float focalLength, const float filmApertureWidth,
 	top = canvasHeight / 2.0;
 	bottom = -top;
 
-	angleOfViewHorizontal = 2 * atan((filmApertureWidth*inchToMm) / (2.0*focalLength));
-	angleOfViewVertical = 2 * atan((filmApertureHeight*inchToMm) / (2.0*focalLength));
-	
+	angleOfViewHorizontal = 2 * 180 / PI * atan((filmApertureWidth*inchToMm) / (2.0*focalLength));
+	angleOfViewVertical = 2 * 180 / PI * atan((filmApertureHeight*inchToMm) / (2.0*focalLength));
+
 	filmAspectRatio = filmApertureWidth / filmApertureHeight;
 
 	printf("Screen window bottom-left, top-right coordinates ,angleOfViewHorizontal ,angleOfViewVertical %f %f %f %f %f %f\n", bottom, left, top, right, angleOfViewHorizontal, angleOfViewVertical);
 
 }
 
-void compute_Screen_RasterSpace(const float left,const float right,
-	const float top,const float bottom,float nearClipingPlane,
-	float imageWidth,float imageHeight,
-	const vec3f pWorld,const Matrix44f worldToCamera, vec2f &pNDC,vec2i &pRaster,bool &visible) {
+void compute_Screen_RasterSpace(const float left, const float right,
+	const float top, const float bottom, float nearClipingPlane,
+	unsigned int imageWidth, unsigned int imageHeight,
+	const vec3f pWorld, const Matrix44f worldToCamera, vec2f &pNDC, vec3i &pRaster, bool &visible) {
 
 	vec3f pCamera;
 	worldToCamera.multVecMatrix(pWorld, pCamera);
@@ -80,12 +80,12 @@ void compute_Screen_RasterSpace(const float left,const float right,
 	if (pScreen.x > right || pScreen.x<left || pScreen.y>top || pScreen.y < bottom)
 		visible = false;
 
-	pNDC.x = (pScreen.x + right) / (2.0*right);
-	pNDC.y = (pScreen.y + top) / (2.0*top);
+	pNDC.x = 2 * pScreen.x / (right - left) - (right + left) / (right - left);
+	pNDC.y = 2 * pScreen.y / (top - bottom) - (top + bottom) / (top - bottom);
 
-	pRaster.x = (int)(pNDC.x*imageWidth);
-	pRaster.y = (int)(pNDC.y*imageHeight);
-
+	pRaster.x = (pNDC.x + 1) / 2 * imageWidth;
+	pRaster.y = (1 - (pNDC.y + 1) / 2)*imageHeight;//NDC y轴与raster y轴相反
+	pRaster.z = -pCamera.z;
 }
 
 //float edgeFunction() {
